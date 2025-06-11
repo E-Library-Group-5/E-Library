5288f021fbb4a7700cd3cc345f5fb43dc9ca53d5
Jeffery Marfo <marfojeffery31@gmail.com>
2025-06-11 02:02:19 +0000

    fixed failed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import Landing from "./pages/Landing";
-import Books from "./pages/books";
+
 import SingleBook from "./pages/SingleBook";
 import AddBook from "./pages/AddBook";
 
@@ -10,13 +10,14 @@ import EventsPage from "./pages/EventsPage";
 import EditBook from "./pages/EditPage";
 import AboutPage from "./pages/About";
 import ContactPage from "./pages/ContactPage";
+import Books from "./pages/books";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/books" element={<Books />} />
+        <Route path="/books" element={<Books/>} />
         <Route path="/singlebook" element={<SingleBook />} />
         <Route path="/addbook" element={<AddBook />} />
         <Route path="/eventspage" element={<EventsPage />} />
